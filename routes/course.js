952cdb3d472c5accc4b0f0ace630c6cb6a1ca880
ashin <ashin.jnv@gmail.com>
2022-04-01 15:38:59 +0530

    nvbn

--- routes/course.js
+++ routes/course.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router()
 
 
-const COURSEDETAILS = require('../models/COURSEDATA');
+const COURSEDETAILS = require('../models/courseData');
 
 
 
--- routes/staff.js
+++ routes/staff.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router()
 
 
-const STAFFDETAILS = require('../models/STAFFDATA');
+const STAFFDETAILS = require('../models/staffData');
 
 
 
