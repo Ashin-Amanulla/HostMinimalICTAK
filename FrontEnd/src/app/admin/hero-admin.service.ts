8e05430f95419b2ef5bea92a5975411c05e09317
ashin <ashin.jnv@gmail.com>
2022-04-23 16:57:10 +0530

    your commit message

--- FrontEnd/src/app/admin/hero-admin.service.ts
+++ FrontEnd/src/app/admin/hero-admin.service.ts
@@ -9,8 +9,8 @@ export class HeroAdminService {
 
   constructor(private http: HttpClient) { }
 
-  // server_address: string ='/api';
-  server_address: string = "http://localhost:8887/api"
+  server_address: string ='/api';
+  // server_address: string = "http://localhost:8887/api"
 
 
   //Course
